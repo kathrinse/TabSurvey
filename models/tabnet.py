7533f3188ef129699735edef3caff5dcfbe26bb7
kathrinse <kathrin.sessler@student.uni-tuebingen.de>
2021-11-15 14:57:15 +0100

    Updated tabnet params

--- models/tabnet.py
+++ models/tabnet.py
@@ -11,10 +11,17 @@ class TabNet(BaseModel):
         # Paper recommends to be n_d and n_a the same
         self.params["n_a"] = self.params["n_d"]
 
+        # Delete batch size from params, as TabNet can not get it as input
+        self.tabnet_params = self.params.copy()
+        del self.tabnet_params["batch_size"]
+
+        #self.batch_size = self.params["batch_size"]
+        #del self.params["batch_size"]
+
         if args.objective == "regression":
-            self.model = TabNetRegressor(**self.params)
+            self.model = TabNetRegressor(**self.tabnet_params)
         elif args.objective == "classification":
-            self.model = TabNetClassifier(**self.params)
+            self.model = TabNetClassifier(**self.tabnet_params)
 
     def fit(self, X, y, X_val=None, y_val=None):
         if self.args.objective == "regression":
@@ -23,8 +30,11 @@ class TabNet(BaseModel):
         elif self.args.objective == "classification":
             metric = ["logloss"]
 
+        print("Batch size", self.params["batch_size"])
+
         self.model.fit(X, y, eval_set=[(X_val, y_val)], eval_name=["eval"], eval_metric=metric,
-                       max_epochs=self.args.epochs, patience=self.args.early_stopping_rounds, batch_size=128)
+                       max_epochs=self.args.epochs, patience=self.args.early_stopping_rounds,
+                       batch_size=self.params["batch_size"])
 
     @classmethod
     def define_trial_parameters(cls, trial, args):
@@ -32,8 +42,11 @@ class TabNet(BaseModel):
             "n_d": trial.suggest_int("n_d", 8, 64),
             "n_steps": trial.suggest_int("n_steps", 3, 10),
             "gamma": trial.suggest_float("gamma", 1.0, 2.0),
+            "cat_emb_dim": trial.suggest_int("cat_emb_dim", 1, 3),
             "n_independent": trial.suggest_int("n_independent", 1, 5),
             "n_shared": trial.suggest_int("n_shared", 1, 5),
-            "momentum": trial.suggest_float("momentum", 0.001, 0.4, log=True)
+            "momentum": trial.suggest_float("momentum", 0.001, 0.4, log=True),
+            "mask_type": trial.suggest_categorical("mask_type", ["sparsemax", "entmax"]),
+            "batch_size": trial.suggest_categorical("batch_size", [64, 128, 256, 512, 1024])
         }
         return params
